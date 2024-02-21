5886b63fb7f9ffe7334407b16a9e7e54c5c06ca6
Eva López <evamlopez1981@gmail.com>
2024-02-21 17:40:50 +0100

    eva estructura de spring-boot

---
+++ dev.hackathon/src/test/java/HomeHub/dev/hackathon/ApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeHub.dev.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
