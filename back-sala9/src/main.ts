dabe8b778d669540a845ec276843989db63ebc76
ctupac999 <critiands9999@gmail.com>
2024-02-22 20:26:42 +0100

    cambios cors

--- back-sala9/src/main.ts
+++ back-sala9/src/main.ts
@@ -1,34 +1,37 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Configurar las opciones CORS
-  const options: CorsOptions = {
-    origin: 'http://localhost:5173/', // Reemplaza con el origen de tu aplicación frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: false,
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-  };
-
-  app.enableCors(options);
-
-  await app.listen(3000); // Reemplaza con el puerto en el que deseas que se ejecute tu aplicación
-}
-bootstrap();
-
-
-
-
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
+
+//   // Configurar las opciones CORS
+//   const options: CorsOptions = {
+//     origin: 'http://localhost:5173/', // Reemplaza con el origen de tu aplicación frontend
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//     credentials: false,
+//     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+//   };
+
+//   app.enableCors(options);
+
+//   await app.listen(3000); // Reemplaza con el puerto en el que deseas que se ejecute tu aplicación
 // }
 // bootstrap();
+
+
+
+
+import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+
+    await app.listen(3000);
+  }
+  bootstrap();
+
