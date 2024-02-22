358ea93f7d6cb299d52571162df8e90231774bf4
ctupac999 <critiands9999@gmail.com>
2024-02-22 19:50:41 +0100

    arreglado con cors

--- back-sala9/src/main.ts
+++ back-sala9/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configurar las opciones CORS
+  const options: CorsOptions = {
+    origin: 'https://tu-aplicacion-frontend.com', // Reemplaza con el origen de tu aplicación frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  };
+
+  app.enableCors(options);
+
+  await app.listen(3000); // Reemplaza con el puerto en el que deseas que se ejecute tu aplicación
 }
 bootstrap();
+
+
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
